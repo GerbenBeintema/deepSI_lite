ec4a5acb8e4b463e54dac431da2ba3abc3cc213a
Gerben Beintema <g.i.beintema@tue.nl>
2024-08-08 14:51:37 +0200

    inclusion of samples per sec unit in fit function

--- deepSI_lite/fitting.py
+++ deepSI_lite/fitting.py
@@ -36,6 +36,7 @@ from copy import deepcopy
 from tqdm.auto import tqdm
 from torch import nn, optim
 from nonlinear_benchmarks import Input_output_data
+import time
 def fit(model: nn.Module, train:Input_output_data, val:Input_output_data, n_its:int, T:int=50, \
         batch_size:int=256, stride:int=1, val_freq:int=500, optimizer:optim.Optimizer=None, loss_fun=compute_NMSE):
     code = token_urlsafe(4).replace('_','0').replace('-','a')
@@ -45,7 +46,7 @@ def fit(model: nn.Module, train:Input_output_data, val:Input_output_data, n_its:
     arrays_val = model.create_arrays(val, T='sim')
     itter = data_batcher(*arrays, batch_size=batch_size)
     best_val, best_model, best_optimizer, loss_acc = float('inf'), deepcopy(model.state_dict()), deepcopy(optimizer.state_dict()), float('nan')
-    NRMS_val, NRMS_train = [], [] #initialize the train and val monitor
+    NRMS_val, NRMS_train, time_usage = [], [], 0. #initialize the train and val monitor
     try:
         for it_count, batch in zip(tqdm(range(n_its)), itter):
             if it_count%val_freq==0: ### Validation ###
@@ -53,16 +54,20 @@ def fit(model: nn.Module, train:Input_output_data, val:Input_output_data, n_its:
                 NRMS_train.append((loss_acc/val_freq)**0.5)
                 if NRMS_val[-1]<=best_val:
                     best_val, best_model, best_optimizer = NRMS_val[-1], deepcopy(model.state_dict()), deepcopy(optimizer.state_dict())
-                cloudpickle.dump({'NRMS_val':np.array(NRMS_val),'last_model':deepcopy(model.state_dict()), 'best_model':best_model, 'best_optimizer':best_optimizer, \
-                                  'NRMS_train':np.array(NRMS_train),'last_optimizer':deepcopy(optimizer.state_dict())}, open(save_filename,'wb'))
-                print(f'it {it_count:7,} NRMS loss {NRMS_train[-1]:.4f} NRMS val {NRMS_val[-1]:.4f}{"!!" if NRMS_val[-1]==best_val else "  "}')
+                cloudpickle.dump({'NRMS_val':np.array(NRMS_val),'last_model':deepcopy(model.state_dict()), 'best_model':best_model, \
+                                  'best_optimizer':best_optimizer, 'NRMS_train':np.array(NRMS_train),\
+                                  'last_optimizer':deepcopy(optimizer.state_dict()), 'samples/sec': (it_count*batch_size/time_usage if time_usage>0 else None)}, \
+                                  open(save_filename,'wb'))
+                print(f'it {it_count:7,} NRMS loss {NRMS_train[-1]:.5f} NRMS val {NRMS_val[-1]:.5f}{"!!" if NRMS_val[-1]==best_val else "  "} {(it_count*batch_size/time_usage if time_usage>0 else float("nan")):.2f} samps/sec')
                 loss_acc = 0.
             ### Train Step ###
+            start_t = time.time()
             loss = loss_fun(model, *batch)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             loss_acc += loss.item()
+            time_usage += time.time()-start_t
     except KeyboardInterrupt:
         print('Stopping early due to KeyboardInterrupt')
     model.load_state_dict(best_model)
